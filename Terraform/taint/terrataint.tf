306b0c746ac42350df26b9651f4cebe9241d8e6f
Sabarish1241 <sabarishcloud410@gmail.com>
2024-04-30 16:08:26 +0530

    adding taint file

---
+++ Terraform/taint/terrataint.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "example1" {
+  ami           = ""
+  instance_type = "t2.micro"
+}
+resource "aws_instance" "example2" {
+  ami           = ""
+  instance_type = "t2.micro"
+}
+

68ec00c567b42e260707de465b8010f5baf6780e
Sowmya1241 <161732360+Sowmya1241@users.noreply.github.com>
2024-04-30 16:43:45 +0530

    Update terrataint.tf

--- Terraform/taint/terrataint.tf
+++ Terraform/taint/terrataint.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "example1" {
-  ami           = ""
+  ami           = "ami-090252cbe067a9e58"
   instance_type = "t2.micro"
 }
 resource "aws_instance" "example2" {
-  ami           = ""
+  ami           = "ami-090252cbe067a9e58"
   instance_type = "t2.micro"
 }
 
